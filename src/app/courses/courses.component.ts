00f04c30e9822f187116fd0bf46daf4b4d11dee0
sali1502 <sali1502@sudent.miun.se>
2024-10-06 19:56:16 +0200

    Filtrering av kurskod och kursnamn med felmeddelande

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -2,23 +2,35 @@ import { Component } from '@angular/core';
 import { Course } from '../models/course';
 import { CoursesService } from '../services/courses.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.css'
 })
 export class CoursesComponent {
   // Properties
   courses: Course[] = [];
+  filteredCourses: Course[] = [];
+  inputValue: string = "";
 
   constructor(private coursesService: CoursesService) { }
 
   ngOnInit() {
     this.coursesService.getCourses().subscribe((courses) => {
       this.courses = courses;
+      this.filteredCourses = courses;
     });
+  }
+
+  // Metod för filtrering
+  filterCourses(): void {
+    this.filteredCourses = this.courses.filter((course) =>
+      course.coursename.toLowerCase().includes(this.inputValue.toLowerCase()) ||
+      course.code.toLowerCase().includes(this.inputValue.toLowerCase())
+    );
   }
 }
